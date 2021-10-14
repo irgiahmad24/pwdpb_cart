e3cd9fd8efc3a20de7623c817d562ee74393063f
irgiahmad24 <irgiahmad9780@gmail.com>
2021-10-14 13:30:20 +0700

    part 4

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -14,10 +14,12 @@ const Navbar = () => {
     {/*links*/}
     <ul className="navbar_links">
         <li>
-            <Link to="/cart">
+            <Link to="/cart" className="cart_link">
              <i className="fas fa-shopping-cart"></i>
+             <span>
               Cart
-             <span className="cartlogo_badge">0</span>
+            <span className="cartlogo_badge">0</span>
+            </span>
             </Link>
         </li>
         <li>
